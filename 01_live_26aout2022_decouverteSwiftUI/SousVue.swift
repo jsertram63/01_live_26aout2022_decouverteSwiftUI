a69f268ad3f7e5a60c1df86f3eb8359ffedc6dbd
jsertram63 <jser>
2022-08-26 15:48:39 +0200

    Initial Commit

---
+++ 01_live_26aout2022_decouverteSwiftUI/SousVue.swift
@@ -0,0 +1,23 @@
+//
+//  SousVue.swift
+//  01_live_26aout2022_decouverteSwiftUI
+//
+//  Created by Lunack on 26/08/2022.
+//
+
+import SwiftUI
+
+struct SousVue: View {
+    
+    @Binding var name:String
+    var body: some View {
+        TextField("Entrez votre nom", text: $name)
+    }
+    
+}
+/*
+struct SousVue_Previews: PreviewProvider {
+    static var previews: some View {
+        SousVue()
+    }
+}*/
---
+++ 01_live_26aout2022_decouverteSwiftUI/_1_live_26aout2022_decouverteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_live_26aout2022_decouverteSwiftUIApp.swift
+//  01_live_26aout2022_decouverteSwiftUI
+//
+//  Created by Lunack on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct _1_live_26aout2022_decouverteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
